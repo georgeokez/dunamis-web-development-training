1f160b1db709ca0f49dbc72dc8c7929a53e60046
georgeokez <georgeokez@gmail.com>
2022-08-07 15:33:36 +0100

    feat: added final website lecture for lesson 4

---
+++ lesson-four-html-and-css/final/script.js
@@ -0,0 +1,13 @@
+// // Get the container Element
+// let menuItems = document.getElementsByClassName("menu-item");
+
+// for(let i = 0; i < menuItems.length; i++){
+
+//     console.log(menuItems[i].innerText);
+
+//     menuItems[i] = addEventListener("click", function(){
+//         let activeMenu = document.querySelector(".active");
+//         activeMenu.classList.remove("active");
+//         this.classList.add("active");
+//     });
+// }
